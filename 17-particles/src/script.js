79f7a56101224b1ff886a89ea6d4a2dc127e9aaf
couvq <quentincouvelaire@gmail.com>
2025-08-26 16:21:48 -0400

    gave particles their own texture

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -1,6 +1,6 @@
 import GUI from "lil-gui";
 import * as THREE from "three";
-import { BufferAttribute, BufferGeometry, Points, PointsMaterial } from "three";
+import { BoxGeometry, BufferAttribute, BufferGeometry, Color, Mesh, MeshBasicMaterial, Points, PointsMaterial } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 /**
@@ -19,6 +19,7 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
 /**
  * Particles
@@ -33,9 +34,18 @@ particlesGeometry.setAttribute('position', new BufferAttribute(positions, 3))
 const particlesMaterial = new PointsMaterial({
   size: 0.1,
   sizeAttenuation: true,
+  color: new Color('#ff88cc'),
+  alphaMap: particleTexture,
+  transparent: true,
+  // alphaTest: 0.001,
+  // depthTest: false // not great if other objects in your scene, was able to see particles behind a cube I added
+  depthWrite: false
 });
 const particles = new Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
+
+const cube = new Mesh(new BoxGeometry(), new MeshBasicMaterial())
+scene.add(cube)
 
 /**
  * Sizes
