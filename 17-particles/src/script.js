c3c16a9ab50dac06d37d975c4f583ab7eb569a9e
couvq <quentincouvelaire@gmail.com>
2025-08-26 16:49:14 -0400

    changing particle colors to be random rgb value

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -1,6 +1,16 @@
 import GUI from "lil-gui";
 import * as THREE from "three";
-import { AdditiveBlending, BoxGeometry, BufferAttribute, BufferGeometry, Color, Mesh, MeshBasicMaterial, Points, PointsMaterial } from "three";
+import {
+  AdditiveBlending,
+  BoxGeometry,
+  BufferAttribute,
+  BufferGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  Points,
+  PointsMaterial,
+} from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 /**
@@ -19,31 +29,34 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-const particleTexture = textureLoader.load('/textures/particles/2.png')
+const particleTexture = textureLoader.load("/textures/particles/2.png");
 
 /**
  * Particles
  */
-const particlesGeometry = new BufferGeometry()
-const count = 20000 // how many partices we want to represent
-const positions = new Float32Array(count * 3) // multiplied by three because each grouping of 3 in the array represents x, y, z coordinates
-for(let i = 0 ; i < count * 3; i++) {
-    positions[i] = (Math.random() - 0.5) * 10 // random position (x, y, z) between -5 & 5
+const particlesGeometry = new BufferGeometry();
+const count = 20000; // how many partices we want to represent
+const positions = new Float32Array(count * 3); // multiplied by three because each grouping of 3 in the array represents x, y, z coordinates
+const colors = new Float32Array(count * 3); // RGB float array where the three values corresponde the the R, G, or B values
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10; // random position (x, y, z) between -5 & 5
+  colors[i] = Math.random();
 }
-particlesGeometry.setAttribute('position', new BufferAttribute(positions, 3))
+particlesGeometry.setAttribute("position", new BufferAttribute(positions, 3));
+particlesGeometry.setAttribute("color", new BufferAttribute(colors, 3));
 const particlesMaterial = new PointsMaterial({
   size: 0.1,
   sizeAttenuation: true,
-  color: new Color('#ff88cc'),
   alphaMap: particleTexture,
   transparent: true,
   // alphaTest: 0.001,
   // depthTest: false // not great if other objects in your scene, was able to see particles behind a cube I added
   depthWrite: false,
-  blending: AdditiveBlending // when pxs overlap the colors are added, can impact performance
+  blending: AdditiveBlending, // when pxs overlap the colors are added, can impact performance
+  vertexColors: true
 });
-const particles = new Points(particlesGeometry, particlesMaterial)
-scene.add(particles)
+const particles = new Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Sizes
